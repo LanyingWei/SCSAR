433b4a49110b8850b9dd123feb90eafb49434a1a
LanyingWei <lanying.v@gmail.com>
2023-08-01 10:32:32 +0800

    File saving function modified

--- R/saveCellTypeRes.R
+++ R/saveCellTypeRes.R
@@ -1,22 +1,16 @@
-#' Save Cell Type Annotation Results
+#' Save Cell Type Results
 #'
-#' This function saves the cell type annotation results for each cluster to an Excel file.
+#' This function saves the cell type results to an Excel file.
 #'
 #' @param cellTypeRes A list containing the annotation results for each cell cluster.
-#' @param path The path where the Excel file will be saved.
+#' @param path The path where the Excel file will be saved. For example, path = "directory/results.xlsx".
 #'
 #' @return None
 #'
 #' @import openxlsx
 
 saveCellTypeRes <- function(cellTypeRes, path) {
-    outFile <- openxlsx::createWorkbook(path)
-    for (i in seq_along(cellTypeRes)) {
-        openxlsx::addWorksheet(outFile, tolower(names(cellTypeRes)[i]))
-        openxlsx::write.xlsx(outFile, 
-                             sheet = i,
-                             x = cellTypeRes[[i]], 
-                             rowNames = TRUE) 
-    }
-    openxlsx::saveWorkbook(outFile)
+    openxlsx::write.xlsx(x = cellTypeRes,
+                         file = path,
+                         rowNames = TRUE)
 }
